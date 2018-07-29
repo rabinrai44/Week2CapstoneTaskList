8a1c6867caf6c04f422ff278e2198881c955ba68
Rabin Rai <rabinrai44@gmail.com>
2018-07-29 11:32:36 -0400

    Added formatted header

--- TaskListMager/Program.cs
+++ TaskListMager/Program.cs
@@ -10,8 +10,21 @@ namespace TaskListMager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PrintHeader();
+            Console.Write("\nWhat would you like to do? ");
             Console.ReadKey();
+        }
+
+        static void PrintHeader()
+        {
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("\tWelcome to the Task Manager!");
+            Console.WriteLine("---------------------------------------------\n");
+            Console.WriteLine(String.Format(" {0, 4} - {1, -4}", 1, "List Tasks"));
+            Console.WriteLine(String.Format(" {0, 4} - {1, -4}", 2, "Add Task"));
+            Console.WriteLine(String.Format(" {0, 4} - {1, -4}", 3, "Delete Task"));
+            Console.WriteLine(String.Format(" {0, 4} - {1, -4}", 4, "Mark Complete"));
+            Console.WriteLine(String.Format(" {0, 4} - {1, -4}", 5, "Quit"));            
         }
     }
 }
